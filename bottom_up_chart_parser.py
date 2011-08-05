18b061744956390e53c4bf174e98e74f4440a9e2
Ghotifish <mace@Marcs-MacBook-Pro.local>
2011-08-05 20:42:50 +0200

    first steps of parse(), added more methods

--- bottom_up_chart_parser.py
+++ bottom_up_chart_parser.py
@@ -10,14 +10,34 @@ class BottomUpChartParser:
         pass
 
     def parse(self, sentence):
-        '''
-        1) Initialize empty chart
-        2) Apply init_rule to each word in input sentence
-        3) Until no more edges are added:
+        edges = None
+        
+        # Tokenize given sentence
+        tokens = self.tokenize(sentence)
+        n = len(tokens)
+        
+        '''1) Initialize empty chart'''
+        chart = Chart(n+1)
+        
+        
+        '''2) Apply init_rule to each word in input sentence'''
+        pos = 0
+        for token in tokens:
+            edges = self.init_rule(token, pos)
+            chart.add_edges() # add add_edges for multiple edges
+            pos += 1
+        
+        '''3) Until no more edges are added:
         - Apply predict_rule everywhere it applies (push to queue)
-        - Apply fundamental rule everywhere it applies (push to queue)
-        4) Return parses
-        '''
+        - Apply fundamental rule everywhere it applies (push to queue)'''
+        
+        
+        '''4) Return parses'''
+        
+        pass
+    
+    def tokenize(self, sentence):
+        ''' Separate sentence into list of tokens '''
         pass
 
     def init_rule(self, token, pos):
@@ -27,4 +47,7 @@ class BottomUpChartParser:
         pass
 
     def fundamental_rule(self, incomplete_edge):
+        pass
+    
+    def generate_parses(self, chart):
         pass
