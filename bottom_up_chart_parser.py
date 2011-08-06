4665b78c9ae848f6333f4ac9bf1d374fd81cc0ec
ghotifish <rumo2093@gmail.com>
2011-08-06 14:22:32 +0200

    finished parser method

--- bottom_up_chart_parser.py
+++ bottom_up_chart_parser.py
@@ -1,52 +1,59 @@
 #!/usr/bin/env python
+from chart import Chart
+from queue import Queue
 
 class BottomUpChartParser:
 
     grammar = None
-    chart = None
-    queue = None
 
     def __init__(self, grammar):
-        pass
+        self.grammar = grammar
 
-    def parse(self, sentence):
-        edges = None
-        
-        # Tokenize given sentence
+    def parse(self, sentence):        
+        ''' Tokenize given sentence '''
         tokens = self.tokenize(sentence)
+        
+        ''' 1) Initialize empty chart and queue '''
         n = len(tokens)
-        
-        '''1) Initialize empty chart'''
         chart = Chart(n+1)
+        queue = Queue()
         
+        ''' 2) Apply init_rule to each word in input sentence '''
+        self.init_rule(tokens)
         
-        '''2) Apply init_rule to each word in input sentence'''
-        pos = 0
-        for token in tokens:
-            edges = self.init_rule(token, pos)
-            chart.add_edges() # add add_edges for multiple edges
-            pos += 1
-        
-        '''3) Until no more edges are added:
-        - Apply predict_rule everywhere it applies (push to queue)
-        - Apply fundamental rule everywhere it applies (push to queue)'''
-        
+        ''' 3) Until no more edges are added '''
+        enough_parses_found = False
+        while not queue.is_empty() and not enough_parses_found:
+            ''' Push queue element to chart '''
+            edge = queue.get_next_element()
+            chart.add_edge(edge)
+            
+            ''' Apply predict_rule everywhere it applies (push to queue) '''
+            self.predict_rule()
+            
+            ''' Apply fundamental rule everywhere it applies (push to queue)'''
+            self.fundamental_rule()
         
         '''4) Return parses'''
+        return self.generate_parses(chart)
         
-        pass
     
     def tokenize(self, sentence):
         ''' Separate sentence into list of tokens '''
-        pass
+        return sentence.split()
 
-    def init_rule(self, token, pos):
+    def init_rule(self, tokens):
+        pos = -1
+        for token in tokens:
+            pos += 1
+            edge = self.init_rule(token, pos)
+            self.queue.add_element(edge)
         pass # pos = start of Edge
 
-    def predict_rule(self, complete_edge):
+    def predict_rule(self):
         pass
 
-    def fundamental_rule(self, incomplete_edge):
+    def fundamental_rule(self):
         pass
     
     def generate_parses(self, chart):
