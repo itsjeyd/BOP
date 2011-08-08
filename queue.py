11498b2e8ec6d6d6cc1b613afd609c64364ea808
Tim Krones <t.krones@gmx.net>
2011-08-08 17:58:32 +0200

    Implemented all methods of class Queue as specified by our framework. Removed subclass FIFOQueue -- we dont need it, standard queues are FIFO and Queue already works like one

--- queue.py
+++ queue.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python
 
 class Queue:
+    ''' This class implements a standard FIFO queue '''
 
-    def get_next_element(self):
-        pass
+    queue = None
 
-    def add_element(self):
-        pass
-    
+    def __init__(self):
+        self.queue = []
 
-class FIFOQueue(Queue):
-    pass
+    def get_next_edge(self):
+        '''
+        Pop first element from queue
+        '''
+        return self.queue.pop(0) if len(self.queue) >= 1 else None
+
+    def add_edge(self, edge):
+        '''
+        Push new edge onto queue
+        '''
+        self.queue.append(edge)
