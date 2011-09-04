8beef5565dc7c65f04376a088e75e93ebf76b14f
Tim Krones <t.krones@gmx.net>
2011-09-04 12:39:24 +0200

    Added missing docstrings to class methods. Got rid of methods print_prod_rule and format_rhs as they had become obsolete. __str__ implements their functionality. Moved __str__ method up in order to seperate magic methods from regular ones.

--- production_rule.py
+++ production_rule.py
@@ -2,37 +2,50 @@
 
 class ProductionRule:
 
-    lhs = ''
-    rhs = []
-    prob = -1.0
+    lhs = '' # Left-hand side of the production rule; a single non-terminal
+    rhs = [] # Right-hand side of the production rule; a list of
+             # terminals or non-terminals
+    prob = -1.0 # Probability of the production rule as specified by
+                # the grammar
 
     def __init__(self, lhs, rhs, prob):
         self.lhs = lhs
         self.rhs = rhs
         self.prob = prob
 
+    def __str__(self):
+        '''
+        Return string representation of the production rule
+        '''
+        return "%s = %s (%s)" % (self.lhs, " ".join(self.rhs), self.prob)
+
     def get_lhs(self):
+        '''
+        Return LHS of the production rule
+        '''
         return self.lhs
 
     def get_rhs(self):
+        '''
+        Return RHS of the production rule
+        '''
         return self.rhs
 
     def get_rhs_element(self, index):
+        '''
+        Return a specific element on the RHS of the production rule
+        '''
         return self.rhs[index]
 
     def get_rhs_length(self):
+        '''
+        Return the number of elements on the RHS of the production
+        rule
+        '''
         return len(self.rhs)
 
     def get_prob(self):
+        '''
+        Return the probability of the production rule
+        '''
         return self.prob
-
-    def print_prod_rule(self):
-        print self.lhs + ' ---> ' \
-            + self.format_rhs() \
-            + ' [' + str(self.prob) + ']'
-
-    def format_rhs(self):
-        return self.rhs[0] if len(self.rhs) == 1 else ' '.join(self.rhs)
-    
-    def __str__(self):
-        return "%s = %s (%s)" % (self.lhs, " ".join(self.rhs), self.prob)
