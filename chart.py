0476c11eda075b6568f6a971c6a4363c2764f2fa
Tim Krones <t.krones@gmx.net>
2011-09-04 18:21:47 +0200

    Added docstring to get_size, get_edges_starting_at and print_chart methods. Added short description of functionality to beginning of docstring of get_edges method. Shortened print_chart a little.

--- chart.py
+++ chart.py
@@ -12,28 +12,34 @@ class Chart:
         self.size = size
 
     def get_size(self):
+        '''
+        Return size of the chart
+        '''
         return self.size
 
     def add_edge(self, edge):
         '''
-        Adds a newly created edge to the appropriate cell of the
-        chart.
+        Add a newly created edge to the appropriate cell of the
+        chart
         '''
         self.chart[edge.get_start()][edge.get_end()].append(edge)
 
     def get_edges(self, i, j):
         '''
+        Return a list of all edges that live in a specific cell of the
+        chart
+
         Every cell of the chart stores not only one edge but a *list*
         of edges; each of these edges spans the input string from i to
         j. In other words, the coordinates of a cell indicate length,
-        start and end point of the edges stored in the cell. As a
-        consequence, this method had to be renamed to reflect the fact
-        that it returns a list of edges (and not just a single edge).
+        start and end point of the edges stored in the cell.
         '''
         return self.chart[i][j]
 
     def get_edges_starting_at(self, i):
         '''
+        Return all edges *starting* at a specific node
+
         The starting point of an edge is indicated by the row of the
         chart it lives in. Therefore, in order to obtain some kind of
         collection of all edges starting at a certain point i, we
@@ -47,6 +53,12 @@ class Chart:
         return [edge for edge in itertools.chain(*self.chart[i])]
 
     def get_edges_ending_at(self, j):
+        '''
+        Return all edges *ending* at a specific node
+
+        The ending point of an edge is indicated by the *column* of
+        the chart it lives in.
+        '''
         edges = []
         for row in self.chart:
             edges.append(row[j])
@@ -54,18 +66,18 @@ class Chart:
 
 
     def print_chart(self):
+        '''
+        Systematically print contents of each cell of the chart
+        '''
         row = 0
         for line in self.chart:
             column = 0
             for cell in line:
                 if len(cell) > 0:
-                    elems = []
-                    for elem in cell:
-                        elems.append(elem.__str__())
-                    cell_str = " | ".join(elems)
+                    cell_str = " | ".join([edge.__str__() for edge in cell])
                 else:
                     cell_str = ""
-                print "%i:%i = %s" % (row,column,cell_str)
+                print "%i:%i : %s" % (row, column, cell_str)
                 column += 1
             print "---------------"
             row += 1
