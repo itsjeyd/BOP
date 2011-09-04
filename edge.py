fbbdd1e1ecdf60fdeb99135592a0f2e4be4603f1
Tim Krones <t.krones@gmx.net>
2011-09-04 17:54:46 +0200

    Added a little more info to comments of class variables. Reordered methods: Magic ones come first, followed by internal and external methods. Added docstring to calc_prob and __str__.

--- edge.py
+++ edge.py
@@ -2,22 +2,64 @@
 
 class Edge:
 
-    start = -1         # Starting node of the edge
-    end = -1           # Ending node of the edge
+    start = -1         # Start node of the edge
+    end = -1           # End node of the edge
     prob = -1.0        # Probability of the edge
-    prod_rule = None   # Object of type ProductionRule
-    dot = -1           # Position of the dot on the RHS
-    complete = False   # Trigger shows whether dot is at end of RHS
-    known_dtrs = None  # List of immediate daughters of type Edge
+    prod_rule = None   # Production rule associated with the edge
+                       # (Object of type ProductionRule)
+    dot = -1           # Position of the dot on the RHS of prod_rule
+    complete = False   # An edge is complete/inactive if the dot is at
+                       # the end of the RHS
+    known_dtrs = None  # List containing Edge instances representing
+                       # immediate daughters of prod_rule that have
+                       # already been found, i.e. all elements
+                       # *before* the dot on the RHS of prod_rule
 
     def __init__(self, start, end, prod_rule, dot, known_dtrs):
         self.start = start
         self.end = end
         self.prod_rule = prod_rule
         self.dot = dot
-        self.prob = self.calc_prob(known_dtrs)
         self.known_dtrs = known_dtrs
+        self.prob = self.calc_prob(known_dtrs)
         self.set_complete()
+
+    def __str__(self):
+        '''
+        Return string representation of edge
+        '''
+        lhs = self.prod_rule.get_lhs()
+        rhs = self.prod_rule.get_rhs()[:]   # slicing creates a copy,
+                                            # allowing manipulation
+        rhs.insert(self.dot, ".")
+        rhs_string = " ".join(rhs)
+        return "%s = %s (%i:%i, %s)" % (lhs, rhs_string, self.start, self.end, self.prob)
+
+
+    ### START internal auxiliary methods ###
+
+    def calc_prob(self, known_dtrs):
+        '''
+        Calculate the probability of the edge as a whole
+
+        The probability of an edge is the product of the probability
+        of its production rule and the probabilities of all its known
+        daughters.
+        '''
+        prob = self.prod_rule.get_prob()
+        for dtr in known_dtrs:
+            prob *= dtr.get_prob()
+        return prob
+
+    def set_complete(self):
+        '''
+        Sets the completeness trigger to True if dot is at the end of the
+        right hand side and keeps it at False if it is not.
+        '''
+        if self.dot == self.prod_rule.get_rhs_length():
+            self.complete = True
+
+    ### START external methods ###
 
     def get_start(self):
         ''' Returns the starting node of the edge '''
@@ -45,12 +87,6 @@ class Edge:
         '''
         return self.dot
 
-    def calc_prob(self, known_dtrs):
-        prob = self.prod_rule.get_prob()
-        for dtr in known_dtrs:
-            prob *= dtr.get_prob()
-        return prob
-
     def is_complete(self):
         '''
         Returns true iff the dot is at the end of the production rule,
@@ -58,25 +94,10 @@ class Edge:
         '''
         return self.complete
 
-    def set_complete(self):
-        '''
-        Sets the completeness trigger to True if dot is at the end of the
-        right hand side and keeps it at False if it is not.
-        '''
-        if self.dot == self.prod_rule.get_rhs_length():
-            self.complete = True
-
     def get_known_dtrs(self):
         '''
         Return list of daughters that have already been found (through
         application of fundamental rule)
         '''
         return self.known_dtrs
-
-    def __str__(self):
-        lhs = self.prod_rule.get_lhs()
-        rhs = self.prod_rule.get_rhs()[:]   # slicing creates a copy, allowing manipulation
-        rhs.insert(self.dot, ".")
-        rhs_string = " ".join(rhs)
-        return "%s = %s (%i:%i, %s)" % (lhs, rhs_string, self.start, self.end, self.prob)
 
